206c668bbad757afbd9c0858de2a110359f60b8c
Said Mandujano <saidnnnn@gmail.com>
2021-05-10 21:53:48 -0500

    primeras 2 estrofas

---
+++ index.js
@@ -0,0 +1,18 @@
+const chalk = require('chalk');
+
+const history = (name) =>{
+
+    console.log(chalk`{cyan So if you're lonely}`);
+    console.log(chalk`{magentaBright You know I'm here waiting for you}`);
+    console.log(chalk`{white I'm just a cross-hair}`);
+    console.log(chalk`{redBright I'm just a shot away from you}`);
+    console.log(chalk`{yellow **************************************************}`);
+    console.log(chalk`{greenBright And if you leave here}`);
+    console.log(chalk`{yellowBright You leave me broken, shattered I lie}`);
+    console.log(chalk`{blueBright I'm just a cross-hair}`);
+    console.log(chalk`{cyanBright I'm just a shot, then we can die}`);
+    console.log(chalk`{blue **************************************************}`);
+}
+
+
+history('said')
